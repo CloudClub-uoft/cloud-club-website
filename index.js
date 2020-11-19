488e760e42046f76e4536e1e02ced2a0eee77fc4
Matthew Mao <matty8100@gmail.com>
2020-11-19 13:31:19 -0800

    fixed missing server start

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = reuquire('express')
+const express = require('express')
 const app = express()
 const port = 80 
+const http = require('http').createServer(app);
 
 app.use(express.static('public'))
+
+http.listen(port, () => {
+});
