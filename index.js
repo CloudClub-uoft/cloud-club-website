dc230d009cfcc352371dad1484fe07ec6d0e39f1
Matthew Mao <matty8100@gmail.com>
2021-01-15 23:41:48 -0800

    implemented JWT token auth

--- index.js
+++ index.js
@@ -1,126 +1,184 @@
 // Packages
 const express = require('express');
+
 const app = express();
 const http = require('http').createServer(app);
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+// bcrypt settings
 const saltRounds = 10;
 const port = 80;
 
+// jsonwebtoken settings
+const jwtKey = 'my_secret_key';
+const jwtExpiry = 300;
+
 // Connect to the database
 const database = mysql.createConnection({
-    host: 'sql2.freemysqlhosting.net',
-    user: 'sql2377507',
-    password: 'lD1%xR6%',
-    port: 3306,
-    database: 'sql2377507'
+  host: 'sql2.freemysqlhosting.net',
+  user: 'sql2377507',
+  password: 'lD1%xR6%',
+  port: 3306,
+  database: 'sql2377507',
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // Dynamic Routes
 // Register GET request
-app.get('/login', (req,res)=> {
-    res.render('login')
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
-app.get('/register', (req,res)=> {
-    res.render('register')
+app.get('/register', (req, res) => {
+  res.render('register');
 });
 
 // Login POST request
-// TO-DO: Session creation
-app.post('/login', (req, res)=> {
-    var email = req.body.email;
-    var password = req.body.password;
-    database.query('SELECT * FROM `logins` WHERE `email`= ?', [email], function(err, result, fields) {
-        if (err) res.status(500).json({'Error': 'Internal Server Error 500'});
-        if (result.length == 1) {
-                bcrypt.compare(password, result[0].password, function(err, result) {
-                    if (result) {
-                        res.status(200).json({'Message': 'Login Sucessful'});
-                    } else {
-                        res.status(401).json({'Message': 'Email not found or password incorrect'});
-                    };
-                });
+app.post('/login', (req, res) => {
+  const { email } = req.body;
+  const { password } = req.body;
+  database.query('SELECT * FROM `logins` WHERE `email`= ?', [email], (err1, result1) => {
+    if (err1) res.status(500).json({ Error: 'Internal Server Error 500' });
+
+    if (result1.length === 1) {
+      bcrypt.compare(password, result1[0].password, (err2, result2) => {
+        if (err2) res.status(500).json({ Error: 'Internal Server Error 500' });
+
+        if (result2) {
+          const token = jwt.sign({ email }, jwtKey, {
+            algorithm: 'HS256',
+            expiresIn: jwtExpiry,
+          });
+
+          res.cookie('token', token, { maxAge: jwtExpiry * 1000 });
+          res.status(200).json({ Message: 'Login Sucessful' });
         } else {
-            res.status(401).json({'Message': 'Email not found or password incorrect'});
+          res.status(401).json({ Message: 'Email not found or password incorrect' });
         }
-    });
+      });
+    } else {
+      res.status(401).json({ Message: 'Email not found or password incorrect' });
+    }
+  });
 });
 
 // Member list GET request
-app.get('/members', (req, res)=> {
-    database.query('SELECT * FROM `clubmembers`', function(err, result, fields) {
-        if (err) res.status(500).json({'Error': 'Internal Server Error 500'});
-        res.status(200).json(result);
-    });
+app.get('/members', (req, res) => {
+  database.query('SELECT * FROM `clubmembers`', (err, result) => {
+    if (err) res.status(500).json({ Error: 'Internal Server Error 500' });
+    res.status(200).json(result);
+  });
 });
-
-// Register GET request
-
-app.get('/register', (req,res)=> {
-    res.render('register')
-});
-
 
 // Registration POST request
-app.post('/register', (req, res)=> {
-    var first = req.body.firstName;
-    var last = req.body.lastName;
-    var email = req.body.email;
-    var password = req.body.password;
-    // TO-DO: email validation
-    database.query('SELECT * FROM `logins` WHERE `email`=?', [email], function(err, result, fields) {
-        if (err) res.status(500).json({'Error': 'Internal Server Error 500'});
-        if (result.length == 0) {
-            if (password.match(/[a-z]/g) && password.match(/[A-Z]/g) && password.match(/[0-9]/g) && password.match(/[^a-zA-Z\d]/g) && password.length >= 8) {
-                bcrypt.hash(password, saltRounds, (err, hash)=> {
-                    if (err) res.status(500).json({'Error': 'Internal Server Error 500'});
-                    database.query('INSERT INTO `logins`(`first-name`, `last-name`, email, password) VALUES (?, ?, ?, ?)', [first, last, email, hash], function(err, result, fields) {
-                        if (err) res.status(500).json({'Error': 'Internal Server Error 500'});
-                        res.status(201).json({'Message': 'Successfully registered'});
-                    });
-                });
-            } else {
-                // TO-DO: specify which requirements are not met
-                res.status(400).json({'Message': 'Password does not meet the requirements'});
-            };
-        } else {
-            res.status(409).json({'Message': 'User already exists'});
-        }
-    });
+app.post('/register', (req, res) => {
+  const first = req.body.firstName;
+  const last = req.body.lastName;
+  const { email } = req.body;
+  const { password } = req.body;
+  // TO-DO: email validation
+  database.query('SELECT * FROM `logins` WHERE `email`=?', [email], (err1, result) => {
+    if (err1) res.status(500).json({ Error: 'Internal Server Error 500' });
+
+    if (result.length === 0) {
+      if (password.match(/[a-z]/g) && password.match(/[A-Z]/g) && password.match(/[0-9]/g) && password.match(/[^a-zA-Z\d]/g) && password.length >= 8) {
+        bcrypt.hash(password, saltRounds, (err2, hash) => {
+          if (err2) res.status(500).json({ Error: 'Internal Server Error 500' });
+
+          database.query('INSERT INTO `logins`(`first-name`, `last-name`, email, password) VALUES (?, ?, ?, ?)', [first, last, email, hash], (err3) => {
+            if (err3) res.status(500).json({ Error: 'Internal Server Error 500' });
+
+            res.redirect(201, '/').json({ Message: 'Successfully registered' });
+          });
+        });
+      } else {
+        // TO-DO: specify which requirements are not met
+        res.status(400).json({ Message: 'Password does not meet the requirements' });
+      }
+    } else {
+      res.status(409).json({ Message: 'User already exists' });
+    }
+  });
 });
 
-//Example API - For more examples, see this repository: https://github.com/CloudClub-uoft/crud-nodejs-mysql
-app.post('/endpoint', (req, res)=> {
-    // Perform some comparison, operation, etc. on the request body, 
-    // depending on what functionality you're trying to acheive.
+app.post('/refresh', (req, res) => {
+  const { token } = req.cookies;
+  if (!token) {
+    res.status(401).json({ Error: 'Unauthorized 401' });
+  }
 
-    // Find the Express documentation:
-    //      Request object: https://expressjs.com/en/4x/api.html#req
-    //      Response object: https://expressjs.com/en/4x/api.html#res
+  let payload;
+  try {
+    payload = jwt.verify(token, jwtKey);
+  } catch (err) {
+    if (err instanceof jwt.JsonWebTokenError) {
+      res.status(401).json({ Error: 'Unauthorized 401' });
+    }
+    res.status(400).json({ Error: 'Bad Request 400' });
+  }
 
-    // Interact with the SQL database using SQL commands via the `database.query()` object.
-    // Find SQL commands documentation here: https://www.w3schools.com/sql/sql_intro.asp
-    // Find the mysql documentation here: https://www.npmjs.com/package/mysql#introduction
-    
-    database.query("SOME COMMAND", ['Some set of values pertaining to the command'], (err, rows)=>{
-        
-        // Return either an error code+object or a success code+object
-        res.status(200).json({
-            'info' : "Some resource info"
-        });
-        //OR
-        res.status(404).json({
-            'error' : "Resource not found!"
-        });
+  const nowUnixSeconds = Math.round(Number(new Date()) / 1000);
+  if (payload.exp - nowUnixSeconds > 30) {
+    res.status(400).json({ Error: 'Bad Request 400' });
+  }
+
+  const newToken = jwt.sign({ username: payload.username }, jwtKey, {
+    algorithm: 'HS256',
+    expiresIn: jwtExpiry,
+  });
+
+  res.cookie('token', newToken, { maxAge: jwtExpiry * 1000 });
+  res.end();
+});
+
+// JWT token check
+app.get('/check', (req, res) => {
+  const { token } = req.cookies;
+  let payload;
+  try {
+    payload = jwt.verify(token, jwtKey);
+  } catch (err) {
+    if (err instanceof jwt.JsonWebTokenError) {
+      res.status(401).json({ Error: 'Unauthorized 401' });
+    }
+    res.status(200).json({ Message: 'User is not logged in' });
+  }
+
+  res.status(200).json({ Message: `${payload.username} is logged in` });
+});
+
+// Example API - For more examples, see this repository: https://github.com/CloudClub-uoft/crud-nodejs-mysql
+app.post('/endpoint', (req, res) => {
+  // Perform some comparison, operation, etc. on the request body,
+  // depending on what functionality you're trying to acheive.
+
+  // Find the Express documentation:
+  //      Request object: https://expressjs.com/en/4x/api.html#req
+  //      Response object: https://expressjs.com/en/4x/api.html#res
+
+  // Interact with the SQL database using SQL commands via the `database.query()` object.
+  // Find SQL commands documentation here: https://www.w3schools.com/sql/sql_intro.asp
+  // Find the mysql documentation here: https://www.npmjs.com/package/mysql#introduction
+
+  database.query('SOME COMMAND', ['Some set of values pertaining to the command'], (err, rows) => {
+    // Return either an error code+object or a success code+object
+    res.status(200).json({
+      info: 'Some resource info',
     });
+    // OR
+    res.status(404).json({
+      error: 'Resource not found!',
+    });
+  });
 });
 
 http.listen(port, () => {
