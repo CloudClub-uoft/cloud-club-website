70ad18435ae28768a00b027fe81f87ed3d47f660
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-03-09 19:21:17 -0500

    Fixing Rebase Failures
    Moved email state-change header script to header partial
    Deleted most CSS pages (they're so small and non-reused, they should be in <style> tags)

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // Dynamic routing
 require('./routes/routing').boot(app, db);
 
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
-
-app.set('layout', 'layouts/layout')
+app.set('layout', 'layouts/layout');
 
 // Static Documentation
 app.use('/api', express.static('docs'));
