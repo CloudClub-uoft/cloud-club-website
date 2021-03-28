709228d20a577dbff74ab3b3c1e214ec1c5f7d50
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-03-28 18:13:57 -0400

    Added Create New Connection

--- config/sql-db.js
+++ config/sql-db.js
@@ -15,10 +15,16 @@ database.connect((err) => {
 
 database.on('error', err => {
   if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      database = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT,
+      });
       database.connect(err2=>{
         if (err2) throw err2;
         console.log(`Reconnected to MySQL database.`)
-      })
+      });
   } else {
       throw err;
   }
