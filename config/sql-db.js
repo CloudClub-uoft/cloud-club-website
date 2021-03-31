aab5cb9b72862616173fe7778852ba9b9f2b48a5
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-03-31 19:41:02 -0400

    Database Reconnect Hotfix
    Changed from const to var

--- config/sql-db.js
+++ config/sql-db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 // Connect to the SQL database
-const database = mysql.createConnection({
+var database = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
