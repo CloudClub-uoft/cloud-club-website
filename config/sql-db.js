7399641034bdec99c9e28caab7c04ed47cb022a2
Jayden Lefebvre <jayden.lefebvre55@gmail.com>
2021-04-06 00:33:31 -0400

    SQL Recursive Object-Reconnect

--- config/sql-db.js
+++ config/sql-db.js
@@ -1,33 +1,27 @@
 const mysql = require('mysql');
 
 // Connect to the SQL database
-var database = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-});
+var database;
+function connect(){
+  database = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+  });
+  database.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected to MySQL database at '${process.env.DB_HOST}:${process.env.DB_PORT}' as user '${process.env.DB_USER}'`);
+  });
+  database.on('error', err => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        connect();
+    } else {
+        throw err;
+    }
+  });
+}
 
-database.connect((err) => {
-  if (err) throw err;
-  console.log(`Connected to MySQL database at '${process.env.DB_HOST}:${process.env.DB_PORT}' as user '${process.env.DB_USER}'`);
-});
-
-database.on('error', err => {
-  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      database = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        port: process.env.DB_PORT,
-      });
-      database.connect(err2=>{
-        if (err2) throw err2;
-        console.log(`Reconnected to MySQL database.`)
-      });
-  } else {
-      throw err;
-  }
-});
+connect();
 
 module.exports = database;
