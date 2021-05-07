3c0936f64b39695bc5ba3cc9c68429d723890f38
Harsimrat <singh21.h@gmail.com>
2021-05-07 03:24:51 -0400

    Added basic new post functionality.

--- routes/POST-newpost.js
+++ routes/POST-newpost.js
@@ -14,6 +14,9 @@
  * @apiError (500) {String} error "Internal Server Error 500"
  */
 
+var Filter = require('bad-words'),
+    filter = new Filter();
+
 module.exports = (app, db) => {
     // New forum post POST request
     app.post('/newpost', (req, res) => {
@@ -23,17 +26,22 @@ module.exports = (app, db) => {
         }
 
         const { subject, body, camefrom } = req.body;
-        if (subject === undefined || body === undefined) {
-            return res.status(400).json({ error: 'Missing fields, check our API docs at cloudclub.ca/api' });
+
+        //profanity check
+        if (filter.isProfane(body) || filter.isProfane(subject)) {
+            //if the post is created from a webpage, then return the forum page.
+            if (camefrom == "webpage") { return res.render('newpost', { 'selected': 'forum', 'title': 'CloudClub | Forum', 'email': req.session.email, 'profane': true }) } else {
+                return res.status(201).json({ message: 'Post ratified. Please keep the content appropriate and courteous.' })
+            }
         }
 
-        if (subject === "" || body === "") {
-            return res.status(400).json({ error: 'Missing fields, check our API docs at cloudclub.ca/api' });
+        if (subject === undefined || body === undefined || subject === "" || body === "") {
+            if (camefrom == "webpage") { return res.redirect('/newpost?tm=Please fill in all the required details&ts=false') } else
+                return res.status(400).json({ error: 'Missing fields, check our API docs at cloudclub.ca/api' });
         }
 
         db.query(`INSERT INTO cloudclub.forum (userid, subject, body) VALUES (${sesh.userid}, '${subject}', '${body}')`, (err) => {
             if (err) { console.log(err); return res.status(500).json({ error: 'Internal Server Error 500' }); }
-            //if the post is created from a webpage, then return the forum page.
             if (camefrom == "webpage") { return res.redirect('/forum') }
             return res.status(201).json({ message: 'Post created successfully.' });
         });
