b3ecaf81bb4dcf6a53e2749f62b1a3332b09aa2e
Harsimrat <singh21.h@gmail.com>
2021-05-07 02:09:04 -0400

    Added new post functionality.

--- routes/POST-newpost.js
+++ routes/POST-newpost.js
@@ -15,21 +15,27 @@
  */
 
 module.exports = (app, db) => {
-  // New forum post POST request
-  app.post('/newpost', (req, res) => {
-    const sesh = req.session;
-    if (!sesh.email) {
-      return res.status(401).json({ error: 'You are not authorized to perform this action.' });
-    }
+    // New forum post POST request
+    app.post('/newpost', (req, res) => {
+        const sesh = req.session;
+        if (!sesh.email) {
+            return res.status(401).json({ error: 'You are not authorized to perform this action.' });
+        }
 
-    const { subject, body } = req.body;
-    if (subject === undefined || body === undefined) {
-      return res.status(400).json({ error: 'Missing fields, check our API docs at cloudclub.ca/api' });
-    }
+        const { subject, body, camefrom } = req.body;
+        if (subject === undefined || body === undefined) {
+            return res.status(400).json({ error: 'Missing fields, check our API docs at cloudclub.ca/api' });
+        }
 
-    db.query(`INSERT INTO cloudclub.forum (userid, subject, body) VALUES (${sesh.userid}, '${subject}', '${body}')`, (err) => {
-      if (err) { console.log(err); return res.status(500).json({ error: 'Internal Server Error 500' }); }
-      return res.status(201).json({ message: 'Post created successfully.' });
+        if (subject === "" || body === "") {
+            return res.status(400).json({ error: 'Missing fields, check our API docs at cloudclub.ca/api' });
+        }
+
+        db.query(`INSERT INTO cloudclub.forum (userid, subject, body) VALUES (${sesh.userid}, '${subject}', '${body}')`, (err) => {
+            if (err) { console.log(err); return res.status(500).json({ error: 'Internal Server Error 500' }); }
+            //if the post is created from a webpage, then return the forum page.
+            if (camefrom == "webpage") { return res.redirect('/forum') }
+            return res.status(201).json({ message: 'Post created successfully.' });
+        });
     });
-  });
 };
