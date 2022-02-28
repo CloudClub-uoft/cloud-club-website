0b0db029bdc44e7fb60b5edf951d276f51d6790b
Lepabryce <Lepasanabryce@gmail.com>
2022-02-27 20:47:40 -0500

    Add latestPostId and latestPostSubject via query

--- routes/GET-profile.js
+++ routes/GET-profile.js
@@ -2,25 +2,39 @@ const defaultImage = `https://cloudclub-iot-blob-dev.s3.ca-central-1.amazonaws.c
 module.exports = (app, db) => {
   app.get('/profile', (req, res) => {
     const sesh = req.session;
+    var numPosts;
+    var latestPostId = -1;
+    var latestPostSubject;
     if (!sesh.email) {
         return res.redirect('/?tm=You Aren\'t Logged In!&ts=false');
     }
-    db.query(`SELECT * FROM cloudclub.profiles WHERE userid='${sesh.userid}'`, (err, result1) => {
-      if (err) {
-          return res.redirect('/?tm=Internal Server Error 500&ts=false');
-      }
-      else {
-        if (result1.length === 0){
-          db.query('INSERT INTO cloudclub.profiles (`profile_path`, `description`, userid) VALUES (?, ?, ?)', [defaultImage, '', sesh.userid], (err2) => {
-            if (err2) { console.log(err2); return res.redirect('/?tm=Internal Server Error 500&ts=false'); }
-            db.query(`SELECT * FROM cloudclub.profiles WHERE userid='${sesh.userid}'`, (err3, result2) => {
-              if (err3) { console.log(err2); return res.redirect('/?tm=Internal Server Error 500&ts=false'); }
-              return res.render('profile', { 'selected': 'profile', 'title': 'CloudClub | My Profile', 'description': result2[0].description,'profile_path': result2[0].profile_path, 'tm' : req.query.tm, 'ts' : req.query.ts});
-            })
-          });
+    db.query(`SELECT COUNT(*) FROM cloudclub.forum WHERE userid='${sesh.userid}'`,(err, forumPosts) => {
+      if (err) {console.log(err); return res.redirect('/?tm=Internal Server Error 500&ts=false');}
+      numPosts = forumPosts[0]['COUNT(*)'];
+      db.query(`SELECT * FROM cloudclub.forum WHERE userid='${sesh.userid}' ORDER BY timestamp DESC LIMIT 1`, (err, latestPost) => {
+        if (err) {console.log(err); return res.redirect('/?tm=Internal Server Error 500&ts=false');}
+        if (numPosts != 0) {
+          latestPostId = latestPost[0]['postid'];
+          latestPostSubject = latestPost[0]['subject'];
         }
-        else return res.render('profile', { 'selected': 'profile', 'title': 'CloudClub | My Profile', 'description': result1[0].description,'profile_path': result1[0].profile_path, 'tm' : req.query.tm, 'ts' : req.query.ts}); 
-      }
+        db.query(`SELECT * FROM cloudclub.profiles WHERE userid='${sesh.userid}'`, (err, result1) => {
+          if (err) {
+              return res.redirect('/?tm=Internal Server Error 500&ts=false');
+          }
+          else {
+            if (result1.length === 0){
+              db.query('INSERT INTO cloudclub.profiles (`profile_path`, `description`, userid) VALUES (?, ?, ?)', [defaultImage, '', sesh.userid], (err2) => {
+                if (err2) { console.log(err2); return res.redirect('/?tm=Internal Server Error 500&ts=false'); }
+                db.query(`SELECT * FROM cloudclub.profiles WHERE userid='${sesh.userid}'`, (err3, result2) => {
+                  if (err3) { console.log(err2); return res.redirect('/?tm=Internal Server Error 500&ts=false'); }
+                  return res.render('profile', { 'selected': 'profile', 'title': 'CloudClub | My Profile', 'latestPostSubject': latestPostSubject, 'latestPostId':latestPostId, 'numPosts': numPosts, 'date': sesh.date, 'email':sesh.email,'first':sesh.first, 'last':sesh.last, 'description': result2[0].description,'profile_path': result2[0].profile_path, 'tm' : req.query.tm, 'ts' : req.query.ts});
+                })
+              });
+            }
+            else return res.render('profile', { 'selected': 'profile', 'title': 'CloudClub | My Profile', 'latestPostSubject': latestPostSubject, 'latestPostId':latestPostId, 'numPosts': numPosts, 'date': sesh.date, 'email':sesh.email,'first':sesh.first, 'last':sesh.last, 'description': result1[0].description,'profile_path': result1[0].profile_path, 'tm' : req.query.tm, 'ts' : req.query.ts}); 
+          }
+        });
+      });
     });
   });
 };
