bc6ee65d542057544372a890d03cc28ac1579e56
Lepabryce <Lepasanabryce@gmail.com>
2022-03-06 17:17:16 -0500

    Verify AWS credentials

--- config/s3.js
+++ config/s3.js
@@ -1,12 +1,18 @@
 const AWS = require('aws-sdk');
+AWS.config.credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+};
+
+//verify credentials
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack);
+});
 
 const s3Client = new AWS.S3({
   region: 'ca-central-1',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-  },
   computeChecksums: true,
 });
+
 
 module.exports = s3Client;
