5e72b93308cec6f1ac34dd8f342ddd14ecbd9dfe
unknown <singh21.h@gmail.com>
2022-05-29 00:44:44 -0400

    Adding prettier and eslint configuratio files. Removing procfile.

--- index.js
+++ index.js
@@ -1,77 +1,88 @@
 // Packages
-const express = require('express');
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const fs = require('fs');
-const fileUpload = require('express-fileupload');
+const express = require("express")
+const app = express()
+const expressLayouts = require("express-ejs-layouts")
+const fs = require("fs")
+const fileUpload = require("express-fileupload")
 
 // Config
-require('dotenv').config();
-const port = process.env.PORT || 80;
+require("dotenv").config()
+const port = process.env.PORT || 80
 
 // Submodules
-require('./config/redis-sessions')(app);
-const s3Client = require('./config/s3');
-const db = require('./config/sql-db');
+require("./config/redis-sessions")(app)
+const s3Client = require("./config/s3")
+const db = require("./config/sql-db")
 
-
-//override required becuase HTML forms do not support PUT/DELETE
-const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
+// override required because HTML forms do not support PUT/DELETE
+const methodOverride = require("method-override")
+app.use(methodOverride("_method"))
 
 // Express setup
-app.use(fileUpload());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(fileUpload())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("public"))
+app.set("view engine", "ejs")
 
 // Dynamic routing
-app.use(expressLayouts);
-app.set('layout', 'layouts/layout');
-require('./routes/routing').boot(app, db, s3Client);
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
+require("./routes/routing").boot(app, db, s3Client)
 
 // Static Documentation
-app.use('/api', express.static('docs'));
+app.use("/api", express.static("docs"))
 
-//The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function(req, res){
-  res.status(404).render('notfound', { 'selected': 'notfound', 'title': 'CloudClub | Error 404'});
-});
+// The 404 Route (ALWAYS Keep this as the last route)
+app.get("*", function (req, res) {
+	res.status(404).render("notfound", {
+		selected: "notfound",
+		title: "CloudClub | Error 404",
+	})
+})
 
 // Start server
-if(process.env.PRODUCTION){
-  try{
-    // SSL Certificates
-    const key = fs.readFileSync(process.env.PRIVKEY_PATH, 'utf8');
-    const cert = fs.readFileSync(process.env.CERTIFICATE_PATH, 'utf8');
-    const ca = fs.readFileSync(process.env.CHAIN_PATH, 'utf8');
-    const credentials = {
-      key: key,
-      cert: cert,
-      ca: ca
-    };
+if (process.env.PRODUCTION) {
+	try {
+		// SSL Certificates
+		const key = fs.readFileSync(process.env.PRIVKEY_PATH, "utf8")
+		const cert = fs.readFileSync(process.env.CERTIFICATE_PATH, "utf8")
+		const ca = fs.readFileSync(process.env.CHAIN_PATH, "utf8")
+		const credentials = {
+			key: key,
+			cert: cert,
+			ca: ca,
+		}
 
-    const httpsport = process.env.HTTPSPORT || 443;
+		const httpsport = process.env.HTTPSPORT || 443
 
-    // HTTPS main server
-    server = require('https').createServer(credentials, app);
-    server.listen(process.env.HTTPSPORT, () => {
-      console.log(`CloudClub server (secure) now listening on ports: HTTP ${port} -> HTTPS ${httpsport}`)
-    });
+		// HTTPS main server
+		let server = require("https").createServer(credentials, app)
+		server.listen(process.env.HTTPSPORT, () => {
+			console.log(
+				`CloudClub server (secure) now listening on ports: HTTP ${port} -> HTTPS ${httpsport}`
+			)
+		})
 
-    //redirect all non-HTTPS incoming traffic to HTTPS on port 443
-    require('http').createServer((req, res) => {
-      res.writeHead(301, {Location: `https://${req.headers.host}${req.url}`});
-      res.end();
-    }).listen(port);
-  } catch (err) {
-    console.log("Could not find certificates!")
-    process.exit(1);
-  }
-}else{
-  server = require('http').createServer(app);
-  server.listen(port, () => {
-    console.log(`CloudClub server (development) now listening on port ${port}`)
-  });
+		// redirect all non-HTTPS incoming traffic to HTTPS on port 443
+		require("http")
+			.createServer((req, res) => {
+				res.writeHead(301, {
+					Location: `https://${req.headers.host}${req.url}`,
+				})
+				res.end()
+			})
+			.listen(port)
+	} catch (err) {
+		console.log("Could not find certificates!")
+		process.exit(1)
+	}
+} else {
+	let server = require("http").createServer(app)
+
+	server.listen(port, () => {
+		console.log(
+			`CloudClub server (development) now listening on port ${port}`
+		)
+	})
 }
