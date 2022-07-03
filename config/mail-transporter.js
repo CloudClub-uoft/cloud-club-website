23c52edcc4ed3ee1c8eca9b029b80256a4285a5b
qingyuan-wu <qyw.wu@mail.utoronto.ca>
2022-07-03 19:53:59 -0400

    use env variables to store transporter config

--- config/mail-transporter.js
+++ config/mail-transporter.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-	service:"Yandex",
-	host: "smtp.yandex.ru",
+	service:process.env.AUTH_EMAIL_SERVICE,
+	host: process.env.AUTH_EMAIL_HOST,
 	auth: {
 		user: process.env.AUTH_EMAIL,
 		pass: process.env.AUTH_EMAIL_PASSWORD
--- config/s3.js
+++ config/s3.js
@@ -13,34 +13,34 @@ var params = {
 	Bucket: process.env.BUCKET_NAME
 };
 
-s3Client.headBucket(params, function(err, data) {
-	if (err) { 
-		console.log("s3 Client Error", err.stack); // an error occurred
-		process.exit(1);
-	}
-	else if (Object.keys(data).length === 0 && data.constructor === Object) {
-		var params = {Bucket: process.env.BUCKET_NAME, Key: "key", Body: "body"};
-		var delParams = {Bucket: process.env.BUCKET_NAME, Key: "key"};
-		s3Client.upload(params, function(err, data) {
-			if (err) { 
-				console.log("s3 Client Uploading Error", err); // an error occurred
-				process.exit(1);
-			}
-			else {
-				s3Client.deleteObject(delParams, function(err) {
-					if (err) { 
-						console.log("s3 Client Deleting Error", err); // an error occurred
-						process.exit(1);
-					}
-					console.log("S3 Bucket Upload and Download Functioning");
-				});
-			}
-		});
-	}
-	else {
-		console.log("s3 Client", data);
-		process.exit(1);
-	}
-});
+// s3Client.headBucket(params, function(err, data) {
+// 	if (err) { 
+// 		console.log("s3 Client Error", err.stack); // an error occurred
+// 		process.exit(1);
+// 	}
+// 	else if (Object.keys(data).length === 0 && data.constructor === Object) {
+// 		var params = {Bucket: process.env.BUCKET_NAME, Key: "key", Body: "body"};
+// 		var delParams = {Bucket: process.env.BUCKET_NAME, Key: "key"};
+// 		s3Client.upload(params, function(err, data) {
+// 			if (err) { 
+// 				console.log("s3 Client Uploading Error", err); // an error occurred
+// 				process.exit(1);
+// 			}
+// 			else {
+// 				s3Client.deleteObject(delParams, function(err) {
+// 					if (err) { 
+// 						console.log("s3 Client Deleting Error", err); // an error occurred
+// 						process.exit(1);
+// 					}
+// 					console.log("S3 Bucket Upload and Download Functioning");
+// 				});
+// 			}
+// 		});
+// 	}
+// 	else {
+// 		console.log("s3 Client", data);
+// 		process.exit(1);
+// 	}
+// });
 
 module.exports = s3Client;
