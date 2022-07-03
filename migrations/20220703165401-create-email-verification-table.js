5e557eaf91608dc9bc05afe09b805610169d0c07
qingyuan-wu <qyw.wu@mail.utoronto.ca>
2022-07-03 13:06:46 -0400

    add email_verification migration file

---
+++ migrations/20220703165401-create-email-verification-table.js
@@ -0,0 +1,38 @@
+"use strict"
+
+var dbm
+var type
+var seed
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+	dbm = options.dbmigrate
+	type = dbm.dataType
+	seed = seedLink
+}
+
+exports.up = function (db, callback) {
+	// Create the email_verification table
+	db.createTable(
+		"email_verification",
+		{
+			email: { type: "string", notNull: true, autoIncrement: false, primaryKey: true },
+			"first-name": { type: "string", notNull: true },
+      "last-name": { type: "string", notNull: true },
+			timestamp: {type: "timestamp", notNull: true, defaultValue: new String("current_timestamp()")},
+			uuid: {type:"string", notNull: true}
+		},
+		callback
+	)
+}
+
+exports.down = function (db, callback) {
+	db.dropTable("email_verification", callback)
+}
+
+exports._meta = {
+	version: 1,
+}
