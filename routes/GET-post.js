99101ea77ed8ef60e4eeb40c11c08b16d4379583
dylanperera <d.perera@mail.utoronto.ca>
2022-09-04 19:48:27 -0400

    Fixed #Comments section to increase when user enters comment and allow for only users logged in, to post.

--- routes/GET-post.js
+++ routes/GET-post.js
@@ -23,13 +23,13 @@ module.exports = (app, db) => {
 		if (!req.query.id) {
 			return res.redirect("/forum");
 		}
-		db.query(`SELECT * FROM cloudclub.forum WHERE postid='${req.query.id}'`, (err, res) => {
+		db.query(`SELECT * FROM cloudclub.forum WHERE postid='${req.query.id}'`, (err, res1) => {
 			if (err) { return res.redirect("/forum?tm=Internal Server Error 500&ts=false"); }
 			db.query(`SELECT * FROM cloudclub.comments WHERE post_id='${req.query.id}'`, (err2, res2) => {
 				if (err2) { return res.redirect("/forum?tm=Internal Server Error 500&ts=false"); }
-				db.query(`SELECT email FROM cloudclub.logins WHERE id='${res[0].userid}'`, (err3, res3) => {
+				db.query(`SELECT email FROM cloudclub.logins WHERE id='${res1[0].userid}'`, (err3, res3) => {
 					if (err3) { console.log(err3); return res.status(500).json({ error: "Internal Server Error 500" }); }
-					return res.render("post", { "selected": "forum", "title": "CloudClub | Forum", "post": res[0], "comments": res2, "email": res3[0].email, "user_id": req.session.userid });
+					return res.render("post", { "selected": "forum", "title": "CloudClub | Forum", "post": res1[0], "comments": res2, "email": res3[0].email, "user_id": req.session.userid });
 				});
 			});
 		});
